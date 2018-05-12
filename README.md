5056c5d2f953909664a5f65a7de52b43c6bcf239
Julien Bouquillon <julien@bouquillon.com>
2018-05-12 04:20:25 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Mermaid API Example
+
+Microservice to embed [mermaid](https://mermaidjs.github.io/) charts in GitHub or whatever static markdown
+
+Use docker and pupetteer to run [mermaid-cli](https://github.com/fardog/mermaid-cli)
+
+Demo : https://mermaid.now.sh
+
+[![Deploy to now](https://deploy.now.sh/static/button.svg)](https://deploy.now.sh/?repo=https://github.com/revolunet/mermaid-api)
+
+Your mermaid graph is available here : `https://mermaid.now.sh/?q=[MERMAID-DATA]`.
+
+⚠ Be sure to use `encodeURIComponent` to encode the data.
+
+![demo chart](chart)
+
+
+[chart]: https://mermaid-api.now.sh/?q=/?width=400&height=600&q=graph%20TD%0AA%5BChristmas%5D%20--%3E%7CGet%20money%7C%20B(Go%20shopping)%0AB%20--%3E%20C%7BLet%20me%20think%7D%0AC%20--%3E%7COne%7C%20D%5BLaptop%5D%0AC%20--%3E%7CTwo%7C%20E%5BiPhone%5D%0AC%20--%3E%7CThree%7C%20F%5BCar%5D%0A
---
+++ index.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const query = require("micro-query");
+const exec = require("node-exec-promise").exec;
+
+const TIMEOUT = 3000;
+
+const getTmpPath = () => `/tmp/${Math.random()}.mmd`;
+
+const readFile = path =>
+  new Promise((res, rej) => {
+    fs.readFile(path, (err, data) => {
+      if (err) rej(err);
+      else res(data.toString());
+    });
+  });
+
+const writeFile = (path, data, opts = "utf8") =>
+  new Promise((res, rej) => {
+    fs.writeFile(path, data, opts, err => {
+      if (err) rej(err);
+      else res();
+    });
+  });
+
+const svgError = `<svg xmlns="http://www.w3.org/2000/svg" width="800px" height="300px" viewBox="0 0 800 300">
+  <text x="10" y="10" font-size="32">Invalid mermaid data :/</text>
+</svg>`;
+
+const timeout = ({ delay = TIMEOUT } = {}) =>
+  new Promise((resolve, reject) => setTimeout(() => reject("timeout"), delay));
+
+const execWithTimeout = cmd => Promise.race([timeout(), exec(cmd)]);
+
+module.exports = async (req, res) => {
+  const params = query(req);
+  const q = params.q;
+  const width = params.width || 800;
+  const height = params.height || 600;
+  if (q) {
+    res.setHeader("Content-Type", "image/svg+xml");
+    const textPath = getTmpPath();
+    const svgPath = `${textPath}.svg`;
+    const write = await writeFile(textPath, q);
+    const cmd = `./node_modules/.bin/mmdc -i ${textPath} -o ${svgPath} -w ${width} -H ${height}`;
+    return execWithTimeout(cmd).then(
+      ({ stdout, stderr }) => readFile(svgPath),
+      err => svgError
+    );
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    return await readFile("./index.html");
+  }
+};
